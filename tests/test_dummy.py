7e1b3f39473ebefbf402c21a317971ceffbf919b
Hjadall <hu.jad95@hotmail.com>
2026-04-22 09:12:41 +0200

    updated deploy

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -3,3 +3,4 @@ def test_pipeline_ready():
 
 
 print("This is a dummy test to verify the test setup is working.")
+print("If this test passes, it means the testing framework is correctly configured.")
